80ebba5a5162576516a7a570c3decc6b39bc180d
Christopher Obando <cobandor@unal.edu.co>
2025-05-03 09:48:58 -0500

    create write_count_words function

--- homework/tests/test_migracion.py
+++ homework/tests/test_migracion.py
@@ -1,7 +1,11 @@
 import os
 
+from ..src.wordcount import main
+
 
 def test_migracion():
+
+    main()
 
     if not os.path.exists("data/output/results.tsv"):
         raise FileNotFoundError("El archivo results.tsv no existe.")
@@ -15,6 +19,3 @@ def test_migracion():
 
     assert results.get("computational", 0) == "3"
     assert results.get("analytics", 0) == "5"
-
-
-
